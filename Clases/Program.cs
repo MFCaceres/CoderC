331cde3de524849b83cc346acb5f437508ba70fa
Martín Cáceres <licaceresmartin@gmail.com>
2022-11-01 20:12:41 -0300

    cambio mios

--- Clases/Program.cs
+++ Clases/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Clases;
 
-Producto producto = new Producto(1);
+Console.ReadLine(); 
+Producto producto = new Producto();
 
 producto.codigo = 1;
 producto.descripcion = "Esta es mi descripcion";
@@ -8,5 +9,19 @@ producto.descripcion = "Esta es mi descripcion";
 Console.WriteLine(producto.codigo);
 
 Console.WriteLine(producto.ToString());
+
+Console.ReadLine();
+
+Usuario usuario = new Usuario();
+
+usuario.nombre = "Martin";
+usuario.email = "licaceresmartin@gmail.com";
+usuario.apellido = "Caceres";
+
+Console.WriteLine(usuario.nombre);
+Console.WriteLine(usuario.email);
+Console.WriteLine(usuario.apellido);
+
+Console.ReadLine();
 
 //Persona persona = new Persona();
